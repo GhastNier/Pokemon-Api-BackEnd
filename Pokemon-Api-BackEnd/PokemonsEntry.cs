c5eadcbaaa702e4ccca97c24fdba51caff5144b4
Michael Savard <89518368+GhastNier@users.noreply.github.com>
2023-02-22 00:04:26 -0500

    Premier Commit

---
+++ Pokemon-Api-BackEnd/PokemonsEntry.cs
@@ -0,0 +1,12 @@
+namespace Pokemon_Api_BackEnd;
+
+public class PokemonsEntry
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
