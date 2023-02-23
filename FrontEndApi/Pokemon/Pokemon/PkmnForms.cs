8bdc8879416b3efb0282295213305973a24c68d5
Michael Savard <michael.savard@beslogic.com>
2023-02-23 14:27:21 -0500

    Setter and Getter Creation

---
+++ FrontEndApi/Pokemon/Pokemon/PkmnForms.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Pokemon;
+
+public class PkmnForms
+{
+    public class PkmnForm
+    {
+        [BsonElement("_id")] public ObjectId Id { get; set; }
+        public string? Name { get; set; }
+        public int Order { get; set; }
+        [BsonElement("form_order")] public int FormOrder { get; set; }
+        [BsonElement("is_default")] public bool IsDefault { get; set; }
+        [BsonElement("is_battle_only")] public bool BattleOnly { get; set; }
+        [BsonElement("is_mega")] public bool Mega { get; set; }
+        [BsonElement("form_name")] public string? FormName { get; set; }
+        public Pokemons? Pokemon { get; set; }
+        public PkmnFormType[] Types { get; set; }
+        public PkmnFormSprites[] Sprites { get; set; }
+        [BsonElement("version_group")] private VersionGroup VersionGroup { get; set; }
+    }
+}
---
+++ FrontEndApi/Pokemon/Pokemon/PokemonMain.cs
@@ -0,0 +1,48 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Pokemon;
+
+public class PokemonMain
+{
+    public ObjectId Id { get; set; }
+    public int Height { get; set; }
+    public int NatDex { get; set; }
+    public string? Name { get; set; }
+    public int Weight { get; set; }
+    public bool Favorite { get; set; }
+    public string? Sprite { get; set; }
+}
+
+public class Pokemons
+{
+    public ObjectId Id { get; set; }
+    public PkmnAbility[]? Abilities { get; set; }
+    public int BaseExperience { get; set; }
+    public PkmnForm[]? Forms { get; set; }
+    public VersionGameIndex[] GameIndices { get; set; }
+    public int Height { get; set; }
+    public PkmnHeldItems[] HeldItems { get; set; }
+    public bool IsDefault { get; set; }
+    public string LocationAreaEncounters { get; set; }
+    public PkmnMoves[] Moves { get; set; }
+    public string Name { get; set; }
+    public int Order { get; set; }
+    [BsonElement("past_types")] public PastType[] PastTypes { get; set; }
+    public PkmnSpecies Species { get; set; }
+    public PkmnSprites Sprites { get; set; }
+    public PkmnStats[] Stats { get; set; }
+    public PkmnType[] Types { get; set; }
+    public int Weight { get; set; }
+    public bool Favorite { get; set; }
+    public int NatDex { get; set; }
+}
+
+
+
+public class PkmnAbility
+{
+    [BsonElement("is_hidden")] public bool IsHidden { get; set; }
+    public int Slot { get; set; }
+    public Abilities.Ability Ability { get; set; }
+}
